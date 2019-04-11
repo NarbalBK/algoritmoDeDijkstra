d9816f9a4aca3d8d0b11cabd8e0a7cab75feab40
Péricles Narbal <periclesnarbal@gmail.com>
2019-04-11 01:26:33 -0300

    versão final

--- ep1_mod.c
+++ ep1_mod.c
@@ -6,16 +6,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAXV 150        //tem que mudar esse negocio
-#define INFINITO 10000  //tem que mudar esse negocio
+#define MAX 200
+#define INFINITO 99999
 
 
 // Prototipos de funções
-//void dijkstra(int vertices, int origem, int destino, int *custos);
 FILE *abreArquivo (char diretorio[100], FILE *arquivo);
 int *alocaLista (int *vertices, int *listaDeAdjacencias);
 void leArquivo (FILE *arquivo, int *vertices, int *arcos, int *origem, int *destino);
 int *preencheLista (FILE *arquivo, int *listaDeAdjacencias, int *vertices, int *arcos, int *destino);
+void dijkstra(int Vert, int Orig, int Dest, int *Valor);
 
 // Inicio da main
 int main(void){
@@ -30,11 +30,14 @@ int main(void){
     char diretorio[100] = "";
     
     arquivo = abreArquivo(diretorio, arquivo);
+    
     leArquivo(arquivo, &vertices, &arcos, &origem, &destino);
+    
     listaDeAdjacencias = alocaLista(&vertices, listaDeAdjacencias);
+    
     listaDeAdjacencias = preencheLista(arquivo, listaDeAdjacencias, &vertices, &arcos, &destino);
     
-    //dijkstra(vertices, origem, destino, custos);
+    dijkstra(vertices, origem, destino, listaDeAdjacencias);
     
     return 0;
 }
@@ -69,7 +72,7 @@ void leArquivo (FILE *arquivo, int *vertices, int *arcos, int *origem, int *dest
 //Inicio de alocaLista
 int *alocaLista (int *vertices, int *listaDeAdjacencias){
     free(listaDeAdjacencias);
-    //aloca o espaço necessário na memoria
+    //aloca o espa√ßo necess√°rio na memoria
     listaDeAdjacencias = malloc(sizeof(int)*(*vertices)*(*vertices));
     for(int i = 0; i <= ((*vertices)*(*vertices)); i++){
         listaDeAdjacencias[i] = -1;
@@ -89,10 +92,74 @@ int *preencheLista (FILE *arquivo, int *listaDeAdjacencias, int *vertices, int *
     }
     fclose(arquivo);
     return listaDeAdjacencias;
-} //Fim de preencheLista
+}
+//Fim de preencheLista
 
 
-
-
-
-
+//Inicio dijkstra
+void dijkstra(int Vert, int Orig, int Dest, int *Valor){
+    int i = 0;            // contador
+    int v = 0;         // auxiliar vÈrtice v
+    int ant[MAX];     // vetor dos predecessores
+    int z[MAX];       // vÈrtices para os quais se conhece o caminho mÌnimo
+    double min;        // auxiliar
+    double dist[MAX]; // custos dos caminhos
+    
+    for(i = 0; i < Vert; i++){
+        if(Valor[i] != -1){
+            ant[i] = Orig-1;
+            dist[i] = Valor[i];
+        }
+        else{
+            ant[i] = -1;
+            dist[i] = INFINITO;
+        }
+        z[i] = 0;
+    }
+    z[Orig-1] = 1;
+    dist[Orig-1] = 0;
+    
+    //Insere o novo vÈrtice em z
+    while(v != Dest-1 && min != INFINITO){
+        min = INFINITO;
+        
+        for(i = 0; i < Vert; i++){
+            if(z[i] == 0){
+                if(dist[i] >= 0 && dist[i] < min){
+                    min = dist[i];
+                    v = i;
+                }
+            }
+        }
+        
+        //Dist‚ncias dos novos vizinhos de z
+        if(min != INFINITO && v != Dest-1){
+            z[v] = 1;
+            for(i = 0; i < Vert; i++)
+                if(z[i] == 0){
+                    if(Valor[v*Vert+i] != -1 && dist[v]+Valor[v*Vert+i] < dist[i]){
+                        dist[i] = dist[v]+Valor[v*Vert+i];
+                        ant[i] = v;
+                    }
+                }
+        }
+    }//Fim de while(v != destino-1 && min != INFINITO)
+    
+    //Imprime caminho mÌnimo de origem a destino
+    if(min == INFINITO)
+        printf("\nNão existe caminho do vertive %d para %d.\n", Orig, Dest);
+    else{
+        printf("\nO Caminho mínimo de %d para o vértice %d é \n",
+               Orig, Dest);
+        
+        i = Dest;
+        printf("%d", i);
+        i = ant[i-1];
+        
+        while(i !=-1) {
+            printf(" <- %d", i+1);
+            i = ant[i];
+        }
+        printf("\nCusto = %d\n", (int)dist[Dest-1]);
+    }
+}//dijkstra
